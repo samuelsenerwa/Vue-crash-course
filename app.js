277a95d78091d2525abb3e67c0b0464fd86d7854
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-07 20:45:16 +0300

    cdn

--- app.js
+++ app.js
@@ -7,7 +7,21 @@ const app = Vue.createApp({
             gender:'male',
             picture:'https://randomuser.me/api/portraits/med/men/75.jpg'
         }
-    }
+    },
+     methods: {
+        async getUser() {
+            const res = await fetch('https://randomuser.me/api')
+            const {results} = await res.json()
+
+            // console.log(results)
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        },
+     },
 })
 
 app.mount('#app')
