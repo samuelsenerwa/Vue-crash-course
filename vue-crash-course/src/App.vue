d182bcd387645ac37a15da6e0facdbd43ec19c33
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-19 23:53:30 +0300

    footer

--- vue-crash-course/src/App.vue
+++ vue-crash-course/src/App.vue
@@ -5,13 +5,14 @@
   <AddTask @add-task="addTask"/>
   </div>
   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
-  
+  <Footer />
   </div>
     
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
@@ -19,6 +20,7 @@ export default {
   name: "App",
   components: {
     Header,
+    Footer,
     Tasks,
     AddTask
   },
