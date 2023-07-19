345f03e612632ac04f20cb0e6ddd90354834e57b
samuelsenerwa <samuelsenerwa@gmail.com>
2023-07-19 23:35:03 +0300

    DELETE

--- vue-crash-course/src/App.vue
+++ vue-crash-course/src/App.vue
@@ -45,9 +45,14 @@ export default {
     
       this.tasks = [...this.tasks, task]
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if(confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE',
+        })
+
+        res.status === 200 ? (this.tasks = this.tasks.filter((task) => task.id !== id)) : alert('Error deleting tasks')
+        
       }
       
     },
